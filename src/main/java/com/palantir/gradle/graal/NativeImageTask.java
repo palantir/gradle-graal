6bfbac354cef7c183c31149db45e2d018703fdfc
Mark Elliot <melliot@palantir.com>
2018-09-05 11:23:58 -0700

    do not use a native-image build server

--- src/main/java/com/palantir/gradle/graal/NativeImageTask.java
+++ src/main/java/com/palantir/gradle/graal/NativeImageTask.java
@@ -48,6 +48,7 @@ public class NativeImageTask extends DefaultTask {
             }
 
             List<String> args = new ArrayList<>();
+            args.add("--no-server");
             args.add("--verbose");
             args.add("-cp");
             args.add(generateClasspathArgument());
