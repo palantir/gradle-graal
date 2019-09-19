1162369a0a505344b7138c189c27a070e085d48a
Carter Kozak <ckozak@ckozak.net>
2019-09-19 13:59:43 -0400

    Remove unnecessary dependencies to fix builds. (#194)
    Remove unnecessary dependencies to fix builds.

--- src/main/java/com/palantir/gradle/graal/ExtractGraalTask.java
+++ src/main/java/com/palantir/gradle/graal/ExtractGraalTask.java
@@ -16,7 +16,6 @@
 
 package com.palantir.gradle.graal;
 
-import com.palantir.logsafe.exceptions.SafeIllegalStateException;
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -65,7 +64,7 @@ public class ExtractGraalTask extends DefaultTask {
     @TaskAction
     public final void extractGraal() {
         if (!graalVersion.isPresent()) {
-            throw new SafeIllegalStateException("extract task requires graal.graalVersion to be defined.");
+            throw new IllegalStateException("extract task requires graal.graalVersion to be defined.");
         }
 
         Project project = getProject();
